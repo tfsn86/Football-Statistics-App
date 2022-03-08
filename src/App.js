597a27488c295c0d5da8d670c089098ade4422c0
tfsn <tfsn86@gmail.com>
2022-03-08 15:04:51 +0100

    initial datafetching from the football api

--- src/App.js
+++ src/App.js
@@ -1,5 +1,59 @@
+import { useEffect, useState } from 'react';
+
+const API_KEY = process.env.REACT_APP_FOOTBALL_API_KEY;
+
 function App() {
-  return <h1>initial commit</h1>;
+  const [loading, setLoading] = useState(true);
+  const [data, setData] = useState([]);
+
+  const fetchData = async () => {
+    setLoading(true);
+
+    try {
+      const response = await fetch(
+        'https://api-football-v1.p.rapidapi.com/v3/standings?season=2021&league=39',
+        {
+          method: 'GET',
+          headers: {
+            'x-rapidapi-host': 'api-football-v1.p.rapidapi.com',
+            'x-rapidapi-key': API_KEY,
+          },
+        }
+      );
+      const responseJSON = await response.json();
+
+      setLoading(false);
+      setData(responseJSON.response[0].league.standings[0]);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  if (loading) {
+    return <h1>{loading}</h1>;
+  }
+
+  return (
+    <main>
+      {data.map((item) => {
+        const {
+          rank,
+          team: { name: team },
+          points,
+        } = item;
+
+        return (
+          <h1 key={rank}>
+            {rank} {team} {points}
+          </h1>
+        );
+      })}
+    </main>
+  );
 }
 
 export default App;
