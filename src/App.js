e444ec81dca53de5c891609c391ac735ed1f13c5
tfsn <tfsn86@gmail.com>
2022-05-04 12:23:57 +0200

    commented out component with stat about players shots on goal because of a bug in the component

--- src/App.js
+++ src/App.js
@@ -23,7 +23,7 @@ function App() {
           <LeagueStandings />
           <TopScorers />
           <TopAssists />
-          <ShotsOnGoal />
+          {/* <ShotsOnGoal /> */}
         </div>
       </div>
     </>
